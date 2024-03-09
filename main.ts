52f5aca26e0cf9b2fc1e68b609d2c12130cc207e
Razia Pervez <160026294+RaziaPervez@users.noreply.github.com>
2024-03-09 11:25:45 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,33 @@
+//project=40
+
+//Album: Write a function called make_album() that builds a Object describing a music album. The function should take in an artist name and an album title, and it should return a Object containing these two pieces of
+//information. Use the function to make three dictionaries representing different albums. Print each return value to show that Objects are storing the album information correctly. Add an optional parameter to
+//make_album() that allows you to store the number of tracks on an album. If the calling line includes a value for the number of tracks, add that value to the album’s Object. Make at least one new function call that includes the number of tracks on an album.
+
+function make_album(artist: string, title: string, track?: number): object{
+    let album :object={
+       artist:title,
+       title:title,
+       //tracks:tracks
+};
+if(track){
+
+album.track = track;
+
+}
+
+return album;
+
+}
+
+// create three albums, one with the optimal twach make_album(artist: string, title: string, tracks?
+
+let album1 = make_album("The Beatles", "A number | undefined")
+
+let album2 = make_album ("Queen", "A Night at the Opera");
+
+let album3 = make_album("Nirvana", "Nevermind", 12);
+
+console.log(album1)
+console.log(album2) 
+console.log(album3)
